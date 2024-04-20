ddd44c4171f0de551cb25f9aee8c1f6dd7bcd424
Sicelumusa <musaqwabe@gmail.com>
2024-04-20 20:34:33 +0200

    Added 2 new features to get user perspectives and publish them

---
+++ src/components/CitizenStories.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const CitizenStories = () => {
+  const [councilors, setCouncilors] = useState([]);
+
+  const settings = {
+    // dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakepoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
+  useEffect(() => {
+    const fetchCouncilors = async () => {
+      try {
+        // Fetch data from backend
+        const response = await fetch(`http://127.0.0.1:8000/crep/councilors/?rating_type=best`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const data = await response.json();
+        setCouncilors(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchCouncilors();
+  }, []);
+
+  return (
+    <div className="perspective">
+      <h2>Best Rated Councilors</h2>
+      <Slider {...settings}>
+        {councilors.map((councilor) => (
+          <div key={councilor.id} className="councilor-item">
+            <h4>Name:  {councilor.names} {councilor.surname}</h4>
+            {/* {selectedProvinceName && <h4>Province:  {selectedProvinceName} </h4>}
+        {selectedMunicipalityName && <h4>Municipality:  {selectedMunicipalityName} </h4>} */}
+        <h4>Ward:  {councilor.ward_number}</h4>
+        <h4>Average Ratings:  {councilor.avg_ratings}</h4>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default CitizenStories;
---
+++ src/pages/Stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CitizenStories from '../components/CitizenStories';
+import '../App.css';
+
+
+function Stories() {
+  return (
+        <div className='stories-container'>
+            <CitizenStories />
+      </div>
+  );
+}
+export default Stories;
