87db0f01e7be8f5d15d3b2f4ea9717394530ac3e
Sicelumusa <musaqwabe@gmail.com>
2024-06-17 18:44:09 +0200

    Moved the Navbar into the header and changed its bg

--- src/App.js
+++ src/App.js
@@ -20,29 +20,31 @@ function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>CREP</h2>
-        { <p>Community Rating and Empowerment Platform</p> }
-      </header>
-        <Router>
-            <NavBar isAuthenticated={isAuthenticated} />
-            <Routes>
-              <Route path="/" exact element={<Home />} />
-              <Route path="/report" element={<Stories />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/my-councilor" element={<MyCouncilor />} />
-              <Route path="/my-profile" element={<MyProfile />} />
-              <Route path="/signup" element={<Signup  isRegistered={isRegistered}  setIsRegistered={setIsRegistered} />} />
-              <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
-              <Route path="/logout" element={<Logout />} />
-              <Route path='/otp-verification' element={<EmailVerification  isVerified={isVerified} setIsVerified={setIsVerified} />} />
-           </Routes>
-        </Router>
-      <footer className="App-footer">
-        <p>Trasparency is Key to African Economic Transformation and Poverty Alleviation</p>
-      </footer>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h2>CREP</h2>
+          { <p>Community Rating and Empowerment Platform</p> }
+          <NavBar isAuthenticated={isAuthenticated} />
+        </header>
+          
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/report" element={<Stories />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/my-councilor" element={<MyCouncilor />} />
+          <Route path="/my-profile" element={<MyProfile />} />
+          <Route path="/signup" element={<Signup  isRegistered={isRegistered}  setIsRegistered={setIsRegistered} />} />
+          <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path='/otp-verification' element={<EmailVerification  isVerified={isVerified} setIsVerified={setIsVerified} />} />
+        </Routes>
+          
+        <footer className="App-footer">
+          <p>Trasparency is Key to African Economic Transformation and Poverty Alleviation</p>
+        </footer>
+      </div>
+    </Router>
   );
 }
 
