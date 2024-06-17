80b9fd8976c1baaff978f5d82ea4dec78d77baa2
Sicelumusa <musaqwabe@gmail.com>
2024-06-17 17:58:16 +0200

    Removed worst and best councilor references

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import BestCouncilors from '../components/BestCouncilors';
-import WorstCouncilors from '../components/WorstCouncilors';
 import UserEdu from '../components/UserEdu';
 import '../styles/Home.css';
 
@@ -8,15 +6,7 @@ import '../styles/Home.css';
 function Home() {
   return (
       <div className='home-container'>
-        <div className='councilors-container'>
-          <div className='best-councilors'>
-            <BestCouncilors />
-          </div>
-          <div className='worst-councilors'>
-            <WorstCouncilors />
-          </div>
-       </div>
-       <div className='eduHeader'><h2>How To Use This App</h2></div>  
+        <div className='councilors-container'></div>
         <UserEdu />
       </div>
   );
