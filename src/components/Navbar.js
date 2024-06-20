f27c2d09c5c9b24a239ecf8d97865c2c8113c5c1
Sicelumusa <musaqwabe@gmail.com>
2024-06-20 22:28:07 +0200

    Modified the navbar files to accomodate toggling for small screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import '../styles/Navbar.css';
 
 
 function NavBar({ isAuthenticated, setIsAuthenticated }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const User = localStorage.getItem('User');
   
-  const User = sessionStorage.getItem('User');
-  
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
 
   return (
     <nav className="navbar">
-      <ul className="navbar-nav-left">
-        <li className="nav-item"><Link to="/">Home</Link></li>
-        <li className="nav-item"><Link to="/report">Public Perspective</Link></li>
+      <div className="navbar-header">
+        <div className="menu-icon" onClick={toggleMenu}>
+          &#9776;
+        </div>
+      <ul className={`navbar-nav ${menuOpen ? 'open': ''}`}>
+        <li className="nav-item"><Link to="/" onClick={closeMenu}>Home</Link></li>
+        <li className="nav-item"><Link to="/report" onClick={closeMenu}>Public Perspective</Link></li>
         {isAuthenticated ? (
           <>
-            <li className="nav-item"><Link to="/contact">Your Voice Matters</Link></li>
+            <li className="nav-item"><Link to="/contact" onClick={closeMenu}>Your Voice Matters</Link></li>
           </>
         ) : null}
-      </ul>
-      <ul className="navbar-nav-right">
-        <li className="nav-item"><Link to="/my-councilor">My Councilor</Link></li>
+        <li className="nav-item"><Link to="/my-councilor" onClick={closeMenu}>My Councilor</Link></li>
         {isAuthenticated ? (
           <>
-            <li className="nav-item"><Link to="/my-profile">My Profile</Link></li>
-            <li className="nav-item"><span>{User}</span></li>
-            <li className="nav-item"><Link to="/signup">Logout</Link></li>
+            <li className="nav-item"><Link to="/my-profile" onClick={closeMenu}><span>{User}</span></Link></li>
+            <li className="nav-item"><Link to="/signup" onClick={closeMenu}>Logout</Link></li>
           </>
         ) : (
           <>
-            <li className="nav-item"><Link to="/signup">Signup</Link></li>
-            <li className="nav-item"><Link to="/login">Login</Link></li>
+            <li className="nav-item"><Link to="/signup" onClick={closeMenu}>Signup</Link></li>
+            <li className="nav-item"><Link to="/login" onClick={closeMenu}>Login</Link></li>
           </>
         )}
       </ul>
+      </div>
     </nav>
   );
 }
