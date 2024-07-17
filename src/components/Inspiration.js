a6ce0061d7878834a11b36402f5932432fbf2af2
Sicelumusa <musaqwabe@gmail.com>
2024-07-17 12:47:13 +0200

    Added images and an image swiper and its styles

--- src/components/Inspiration.js
+++ src/components/Inspiration.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import '../styles/Home.css';
-import feedback  from '../assets/images/feedback.jpg'
 import { Link } from 'react-router-dom';
+
+import feedback  from '../assets/images/feedback.jpg'
+import education from '../assets/images/education-3670453_1920.jpg'
+import poverty from '../assets/images/poverty-509601_1920.jpg'
+import road from '../assets/images/road-414462_1920.jpg'
+import africa from '../assets/images/africa-1783786_1920.jpg'
+
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCube, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/effect-cube';
+import 'swiper/css/pagination';
 
 
 const Inspiration = () => {
@@ -15,7 +27,35 @@ const Inspiration = () => {
             <p>Get Your Free Account: <Link to="/signup"><button>Register</button></Link></p>
           </div>
           <div className='image-swiper'>
-            <img src={feedback} alt="" className='image' />
+            <Swiper
+              effect={'cube'}
+              grabCursor={true}
+              cubeEffect={{
+                shadow: true,
+                slideShadows: true,
+                shadowOffset: 20,
+                shadowScale: 0.94,
+              }}
+              pagination={true}
+              modules={[EffectCube, Pagination]}
+              className='inspImages'
+            >
+              <SwiperSlide>
+                <img src={feedback} alt="" className='image' />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={africa} alt="" className='image' />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={education} alt="" className='image' />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={poverty} alt="" className='image' />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={road} alt="" className='image' />
+              </SwiperSlide>
+            </Swiper>
           </div>
       </div>
   );
