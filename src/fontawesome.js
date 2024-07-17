c3632ecf6de95a96a0f3e32d7f965bbef47a0961
Sicelumusa <musaqwabe@gmail.com>
2024-07-17 10:52:16 +0200

    Added fontawesome config

---
+++ src/fontawesome.js
@@ -0,0 +1,5 @@
+import { library } from 'fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fab, faTwitter,faLinkedin, faGithub);
