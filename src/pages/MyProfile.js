66272ab2bbe6f62413b62f2e9188cf7932ac46a4
Sicelumusa <musaqwabe@gmail.com>
2024-07-18 20:19:23 +0200

    Added user profile table

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,18 +1,73 @@
-import React from 'react';
-import '../App.css';
+import React, { useState, useEffect } from 'react';
+import { axiosInstance } from '../axiosConfig';
+import '../styles/MyProfile.css';
 
-const MyProfile = () => {
+const MyProfile = ({ isAuthenticated }) => {
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      // fetch user info from backend
+      axiosInstance.get('/auth/profile/')
+        .then(response => {
+          setUser(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching user info:', error);
+      });
+    }
+  }, [isAuthenticated]);
+
   return (
-    <div className='login'>
-      {/* <h2>Login To Be Able To Send Your Ratings</h2>
-      <form>
-	      
-        <input id='username' type='email' placeholder='Enter Your Email Address'></input>
-	      
-        <input id='password' type='password' placeholder='Enter Your Password'></input>
-        <button type='submit'>Login</button>
-      </form> */}
-      <h2>Coming Soon...</h2>
+    <div className='user-profile'>
+      <h2>My Details</h2>
+      <table>
+            <thead>
+               <tr>
+                <td>Field</td>
+                <td>Value</td>
+               </tr>
+             </thead>
+             <tbody>
+                <tr>
+                 <td>Email</td>
+                 <td>{user?.email}</td>
+                </tr>
+                <tr>
+                 <td>Firstname</td>
+                 <td>{user?.first_name}</td>
+                </tr>
+                <tr>
+                 <td>Lastname</td>
+                 <td>{user?.last_name}</td>
+                </tr>
+                <tr>
+                 <td>Username</td>
+                 <td>{user?.username}</td>
+                </tr>
+                <tr>
+                 <td>Province</td>
+                 <td>{user?.province}</td>
+                </tr>
+                <tr>
+                 <td>Municipality</td>
+                 <td>{user?.municipality}</td>
+                </tr>
+                <tr>
+                 <td>Ward_number</td>
+                 <td>{user?.ward_number}</td>
+                </tr>
+                <tr>
+                 <td>Section_or_area</td>
+                 <td>{user?.section_or_area}</td>
+                </tr>
+                <tr>
+                 <td>Password</td>
+                 <td>{user?.password}</td>
+                </tr>
+             </tbody> 
+        </table> 
     </div>
   );
 };
