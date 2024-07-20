71efa0bf8414a10726043d2a4690d4b42e8fe5ac
Sicelumusa <musaqwabe@gmail.com>
2024-07-20 13:10:46 +0200

    Added react-youtube package and created related componet and styling

--- src/components/UserEdu.js
+++ src/components/UserEdu.js
@@ -1,9 +1,34 @@
 import React from "react";
+import YouTube from 'react-youtube';
 import '../styles/Home.css';
 
 const UserEdu = () => {
+  const videoId = 'Dmt9OdGrqEc';
+  
+  const opts ={
+    height: '250',
+    width: '400',
+    playerVars: {
+      autoplay: 0,
+    },
+  };
+
   return (
-    <div >User Education Video Goes Here</div>
+    <div className="user-Edu">
+      <h2>User Education</h2>
+      <div className="councilor-role">
+        <div className="video">
+          <YouTube videoId={videoId} opts={opts} />
+        </div>
+        <h3>Make Informed Decisions When Rating</h3>
+      </div>
+      <div className="app-navigation">
+        <h3>Navigate The Platform</h3>
+        <div className="video">
+          <YouTube videoId={videoId} opts={opts} />
+        </div>
+      </div>
+    </div>
   )
 }
 
