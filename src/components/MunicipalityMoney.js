4789c7306de58fb344c2daf6f623986a9e4d84d1
Sicelumusa <musaqwabe@gmail.com>
2024-07-20 13:15:08 +0200

    Changed the header size

--- src/components/MunicipalityMoney.js
+++ src/components/MunicipalityMoney.js
@@ -5,7 +5,7 @@ import '../styles/Home.css';
 const MunicipalityMoney = () => {
   return (
       <div className='money-container'>
-        <h1>Infrastructural Projects In Your Municipality</h1>
+        <h2>Infrastructural Projects In Your Municipality</h2>
         <p>I am a passionate and dedicated software engineer with hands-on experience in both front-end and back-end development. Through a rigorous 12-month software engineering program at ALX, I have developed a comprehensive skill set that includes C, Python, HTML, CSS, JavaScript, Bootstrap, Sass, jQuery, Django, Node.js and React. My journey has been marked by numerous independent and collaborative projects, each contributing to my growth and proficiency in creating efficient, user-friendly applications. My philosophy, "Place me in any scenario, and I will not only adapt but also excel," reflects my commitment to continuous learning and excellence in every challenge I undertake</p>
       </div>
   );
