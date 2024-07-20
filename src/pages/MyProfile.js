d85b436944a3d8a5269c167c273600afe7ca32cd
Sicelumusa <musaqwabe@gmail.com>
2024-07-20 22:52:50 +0200

    Added some error handling to render the data even if its null

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -37,35 +37,35 @@ const MyProfile = ({ isAuthenticated }) => {
              <tbody>
                 <tr>
                  <td>Email</td>
-                 <td>{user.email}</td>
+                 <td>{user?.email || 'Not Available'}</td>
                 </tr>
                 <tr>
                  <td>Firstname</td>
-                 <td>{user.first_name}</td>
+                 <td>{user?.first_name || 'Not Available'}</td>
                 </tr>
                 <tr>
                  <td>Lastname</td>
-                 <td>{user.last_name}</td>
+                 <td>{user?.last_name || 'Not Available'}</td>
                 </tr>
                 <tr>
                  <td>Username</td>
-                 <td>{user.username}</td>
+                 <td>{user?.username || 'Not Available'}</td>
                 </tr>
                 <tr>
                  <td>Province</td>
-                 <td>{user.province}</td>
+                 <td>{user?.province || 'Not Available'}</td>
                 </tr>
                 <tr>
                  <td>Municipality</td>
-                 <td>{user.municipality}</td>
+                 <td>{user?.municipality || 'Not Available'}</td>
                 </tr>
                 <tr>
                  <td>Ward_number</td>
-                 <td>{user.ward_number}</td>
+                 <td>{user?.ward_number || 'Not Available'}</td>
                 </tr>
                 <tr>
                  <td>Section_or_area</td>
-                 <td>{user.section_or_area}</td>
+                 <td>{user?.section_or_area || 'Not Available'}</td>
                 </tr>
              </tbody> 
         </table> 
